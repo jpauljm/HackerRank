7ba06ba288134bbd318903fb8764df82bc1789c5
Jean Paul Marinho <me@akanj.com>
2023-02-11 19:07:16 -0300

    Improved MinMaxSum using Swift

--- Swift/MinMaxSum.swift
+++ Swift/MinMaxSum.swift
@@ -1,12 +1,14 @@
 //Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
 
-func miniMaxSum(arr: [Int]) -> Void {
-    let sortedArr = arr.sorted()
-    let minimumSum = sortedArr[0]+sortedArr[1]+sortedArr[2]+sortedArr[3]
-    let maximumSum = 
-    sortedArr[sortedArr.count-1] +
-    sortedArr[sortedArr.count-2] +
-    sortedArr[sortedArr.count-3] +
-    sortedArr[sortedArr.count-4]
+func minMaxSum(arr: [Int]) -> Void {
+    let numberOfItems = 4
+    let ascArr = arr.sorted()
+    let descArr = Array(ascArr.reversed())
+    var minimumSum = 0
+    var maximumSum = 0
+    for index in 0..<numberOfItems {
+        minimumSum += ascArr[index]
+        maximumSum += descArr[index]
+    }
     print("\(minimumSum) \(maximumSum)")
 }
